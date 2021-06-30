b8c9f933fbba6b99c01645f6ddb9106f628fbdba
Maksim Shalaev <maksimshalaev11@gmail.com>
2021-06-30 14:15:06 +0300

    v1

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,18 @@
+package ru.netology.domain;
+
+public class Post {
+    private String id; // id пользователя опубликовавшего пост
+    private String name; // имя пользователя опубликовавшего пост
+    private String imageAvatarUrl; // аватарка пользователя опубликовавшего пост
+    private String dataPost; // дата публикации поста
+    private int timePost; // время публикации поста
+    private String imagePostUrl; // содержимое поста
+    private String favorites; // возможность добавить пост в "избранное"
+    private int numberOfLikes; // количество "лайков"
+    private String comments; // комментарии к посту
+    private String toShare; // возможность поделиться данным постом
+    private int views; // количество просмотров поста
+
+
+
+}
