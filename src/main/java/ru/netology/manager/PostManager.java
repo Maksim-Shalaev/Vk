4ccd6beac576c22a3d3e639acb115e8d526bc6b4
Maksim Shalaev <maksimshalaev11@gmail.com>
2021-07-01 15:26:39 +0300

    added PostManager

--- src/main/java/ru/netology/manager/PostManager.java
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -1,4 +1,15 @@
 package ru.netology.manager;
 
-public class PostManager {
+import ru.netology.domain.Post;
+
+public class PostManager { // создан класс PostManager, к-ый обращается к классу Post за хар-ми (полями)
+    private Post[] posts;
+
+    public Post[] search(int ownerId, String domain, String query, int ownersOnly, int count, int offset){ // должен возвращать массив
+      return null;
+    }
+
+    public void delete(int postId, int ownerId){ // ничего не возвращает
+    }
 }
+
